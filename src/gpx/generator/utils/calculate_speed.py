d8a34f18e3bd5eae5c048cea5f233de70df8d63d
4fra <franio.grzywaczewski@gmail.com>
2025-03-14 09:37:37 +0100

    refactor: improve speed calculation logic based on elevation change

--- src/gpx/generator/utils/calculate_speed.py
+++ src/gpx/generator/utils/calculate_speed.py
@@ -1,8 +1,8 @@
 def calculate_speed(avg_speed, elevation_change):
     if elevation_change > 0:
         # Uphill: slower speed
-        speed = avg_speed * (1 - min(elevation_change / 100, 0.5))  # reduce speed by up to 50%
+        speed = avg_speed * (1 - min(elevation_change * 10 / 100, 0.85))
     else:
         # Downhill: faster speed
-        speed = avg_speed * (1 + min(abs(elevation_change) / 100, 0.3))  # increase speed by up to 30%
+        speed = avg_speed * (1 + min(abs(elevation_change) * 10 / 100, 1.15))
     return speed
